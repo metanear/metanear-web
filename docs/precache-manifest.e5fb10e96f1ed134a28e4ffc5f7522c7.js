250329b872d93af211c520499b0c9b5192f87f9b
Evgeny Kuzyakov <ek@nearprotocol.com>
2020-01-30 11:42:06 -0800

    Add docs

---
+++ docs/precache-manifest.e5fb10e96f1ed134a28e4ffc5f7522c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f989a2c8073d648582d87bdb98f9a2c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fd194ceabfdd4b205a0",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "9ddbc7393bfc87a721a9",
+    "url": "/static/css/main.eb02feb5.chunk.css"
+  },
+  {
+    "revision": "5fd194ceabfdd4b205a0",
+    "url": "/static/js/2.a1854077.chunk.js"
+  },
+  {
+    "revision": "1efbb6f8ece037c6104976f3a849caf0",
+    "url": "/static/js/2.a1854077.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ddbc7393bfc87a721a9",
+    "url": "/static/js/main.a6cceb47.chunk.js"
+  },
+  {
+    "revision": "b934ee90226235cf27dc",
+    "url": "/static/js/runtime-main.b70c9756.js"
+  },
+  {
+    "revision": "55517b93c50499eda5108ca08447bb25",
+    "url": "/static/media/gray_near_logo.55517b93.svg"
+  }
+]);
